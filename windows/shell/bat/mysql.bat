89580264d5f7cb509d4c0daee5a18a4470b11645
今天幸福不在家 <zhujwmail@163.com>
2024-04-18 13:29:08 +0800

    update

--- windows/shell/bat/mysql.bat
+++ windows/shell/bat/mysql.bat
@@ -5,22 +5,17 @@
 :start
 mode con cols=55 lines=20
 title mysql小工具
-echo ************************************************
-echo  1.开启mysql    2.关闭mysql    3.登录    4.退出  
-echo ************************************************
+echo *************************************
+echo  1.开启mysql    2.关闭mysql    3.退出
+echo *************************************
 echo.
 set /p option=请输入你的选择:
 echo.
 if "%option%"=="1" net start mysql
 if "%option%"=="2" net stop mysql
-if "%option%"=="3" (
-mode con cols=100% lines=100%
-title mysql commond
-mysql -uroot -p --prompt="\u@\h \d>"
-)
-if "%option%"=="4" goto byebye
-:: 等待5S回到菜单
-timeout /t 5
+if "%option%"=="3" goto byebye
+:: 等待3S回到菜单
+timeout /t 3
 cls
 set option=null
 goto start
